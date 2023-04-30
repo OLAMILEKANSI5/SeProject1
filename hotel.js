076b0d12d0238a8e251964875fef11e59cd88a2a
JAMES OLALEKAN SAMUEL <66016793+OLAMILEKANSI5@users.noreply.github.com>
2023-04-30 14:55:10 +0100

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,32 @@
+function showDropdown(){
+    var dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
+
+/* CLOSE THE DROPOWN IF THE USER CLICKS OUTSIDE OF IT */
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdown = document.getElementById("myDropdown");
+      if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+      }
+    }
+  }
+  
+  
+  // Search query
+  const form = document.querySelector('#search-form');
+  const searchInput = document.querySelector('#search-input');
+  
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // prevent the form from submitting
+  
+    const searchQuery = searchInput.value;
+    window.location.href = 'https://hotels.ng/hotels-in-' + searchQuery;
+    // searchInput.value = "";
+  });
